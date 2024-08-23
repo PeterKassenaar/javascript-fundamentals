c2e3f34bb87d94e0869378e68b81312e3dc635a4
PeterKassenaar <info@kassenaar.com>
2024-08-23 14:40:56 +0200

    Added example code

---
+++ examples/script/03_casting.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+let count = 'Hello';
+count = count + 1;
+alert(count); // No problem (unlike other languages)!
+
+/*
+const myText = 'Hello Peter'  // single quotes - accepted
+const myText2 = "Hello Peter" // double quotes - also fine
+*/
